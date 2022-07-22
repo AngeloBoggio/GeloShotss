352896c19e1b03bdd7b60e73098e5d56a144a8a7
AngeloBoggio <91862454+AngeloBoggio@users.noreply.github.com>
2022-07-21 23:49:43 -0400

    Add files via upload

---
+++ myGallery.js
@@ -0,0 +1,40 @@
+function myFunction() {
+
+    var x = document.getElementById("myTopnav");
+
+    if(x.className == "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+    
+}
+
+const images = document.querySelectorAll(".gallery-item .image img");
+let imgSrc;
+// get images src onclick
+console.log(images);
+
+images.forEach((img) => {
+    img.addEventListener("click", (e) =>{
+        imgSrc = e.target.src;
+        console.log(imgSrc);
+        const image = document.querySelector(".lifeGuard");
+        image.src = imgSrc;
+        const container = document.querySelector(".modal");
+        container.classList.remove("hide");
+        window.scrollTo(0,0);
+    });
+});
+
+const button = document.querySelector(".button");
+button.addEventListener("click", () => {
+    const container = document.querySelector(".modal");
+    container.classList.add("hide");
+})
+
+document.querySelector(".modal").addEventListener("click", () => {
+    const container = document.querySelector(".modal");
+    container.classList.add("hide");
+})
+
